5cd0c2ad0669c8987d9c427f8427871cd505cda6
Matt <matt@apogee.guru>
2025-05-12 17:06:57 +0100

    chore: update terraform configs

--- infra/dns.tf
+++ infra/dns.tf
@@ -3,10 +3,20 @@ resource "azurerm_dns_zone" "root" {
   resource_group_name = azurerm_resource_group.main.name
 }
 
+resource "null_resource" "dns_settle" {
+  depends_on = [azurerm_dns_zone.root]
+
+  provisioner "local-exec" {
+    command = "sleep 30"
+  }
+}
+
 resource "azurerm_dns_cname_record" "www" {
   name                = "www"
   zone_name           = azurerm_dns_zone.root.name
-  resource_group_name = azurerm_dns_zone.root.resource_group_name
+  resource_group_name = azurerm_resource_group.main.name
   ttl                 = 300
   record              = azurerm_static_web_app.site.default_host_name
+
+  depends_on = [null_resource.dns_settle]
 }
--- infra/static_web_app.tf
+++ infra/static_web_app.tf
@@ -1,5 +1,5 @@
 resource "azurerm_static_web_app" "site" {
-  name                = "swapp-vibe"
+  name                = "swapp-vibe-ntv-ooga"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   sku_size            = "Standard"
